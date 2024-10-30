8bcab6376c144d672e1b93643f5e0d182ed77cbf
pranavdevadas <pranavdevadas2@gmail.com>
2024-10-30 18:20:56 +0530

    debugged for production

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-if (process.env.NODE_ENV == "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => res.send("Server is ready"));
-}
+app.get("/", (req, res) => res.send("Server is ready"));
 
 app.use(notfound);
 app.use(errorHandler);
